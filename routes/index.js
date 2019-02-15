669a3cab3dfa531eb131387ec76a1ddbba46048f
AmritGrewal001 <grewal13amrit@gmail.com>
2019-02-14 21:24:41 -0500

    MY SECOND COMMIT
    MY COMMIT AFTER CREATING 5 PAGES AND ADDING ROUTES

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const rubrics = ['-Use the Express Generator to create a new site called lab2', '-Install nodemon and change the script in package.json to using nodemon instead of node', '-Run application (Make sure it\'s using nodemon)'];
+    const rubrics = ['Front End Web Developer', '55% DESIGNER', '45% CODER'];
     const params = {
-        title: 'Assignment 1',
+        title: 'Amritpal Singh Grewal',
         rubrics
     };
   res.render('index', params);
